b42ebb9edb6b187d6b53e1baa98e6ce85980f10e
Tatiana Chebotar <57943361+tatacheba@users.noreply.github.com>
2024-12-13 15:39:56 +0100

    Good

---
+++ backend/src/main/java/myProject/contactapp/dao/ContactRepository.java
@@ -0,0 +1,8 @@
+package myProject.contactapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import myProject.contactapp.entity.Contact;
+
+public interface ContactRepository 
+  extends JpaRepository<Contact, Long> {}
+  
---
+++ backend/src/main/java/myProject/contactapp/env/SettingApp.java
@@ -0,0 +1,11 @@
+package myProject.contactapp.env;
+
+final public class SettingApp {
+  private static final String HOST = "http://localhost:3000";
+
+  public static String getHost() {
+    return HOST;
+  }
+  
+     
+}
