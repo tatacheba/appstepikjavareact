0b0af63bb3248b0d26d4b1a2866cf73ef92d8f00
Tatiana Chebotar <57943361+tatacheba@users.noreply.github.com>
2024-12-16 15:12:10 +0100

    backend change on mock

--- backend/src/main/java/myProject/contactapp/env/SettingApp.java
+++ backend/src/main/java/myProject/contactapp/env/SettingApp.java
@@ -1,7 +1,9 @@
 package myProject.contactapp.env;
 
 final public class SettingApp {
-  private static final String HOST = "http://localhost:3000";
+  // private static final String HOST = "http://localhost:3000";
+  private static final String HOST = "https://contactapp-phi.vercel.app";
+
 
   public static String getHost() {
     return HOST;
